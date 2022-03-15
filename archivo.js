6d2031f2272b720bb2420d22b7ffaebaf89d83d2
Javier Olivieri <cosoligroup@gmail.com>
2022-03-15 13:44:11 -0300

    prueba

---
+++ archivo.js
@@ -0,0 +1,11 @@
+let edad = parseInt(prompt("cual es tu edad?"));
+
+function edads () {
+    if (edad > 18 && edad < 40 ) {
+        alert("puedes ingresar");
+    } else {
+        alert("no puedes ingresar");
+    }
+}
+
+edads ()
